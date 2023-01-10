fc21a447f0365d360e3328afd408b796f9c2fadb
Sallie McKee <salliemckee7@gmail.com>
2023-01-10 12:47:18 -0600

    finished styling sign-in page

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,15 +1,7 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-
-import {
-  auth, //auth is a singleton!
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
-} from "../../utils/firebase/firebase.utils";
-
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+
+import "./authentication.styles.scss";
 
 const Authentication = () => {
   // this is an example of signInWithGoogleRedirect
@@ -25,8 +17,7 @@ const Authentication = () => {
   // }, []);
 
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className="authentication-container">
       <SignInForm />
       {/* <button onClick={signInWithGoogleRedirect}>
         Sign in with google redirect
