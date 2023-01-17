b6b034eb849668cde4af2b514b9781ee1937a8bd
Sallie McKee <salliemckee7@gmail.com>
2023-01-17 14:25:16 -0600

    rearranging shop page and setting up category collection

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState, useEffect } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+export const CategoriesContext = createContext({
+  categoriesMap: [],
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    //any async things you need to do inside a useEffect, wrap in an async function
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
