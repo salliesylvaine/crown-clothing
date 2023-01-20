297af00193bc2f0d39c1775e544c4ce86c4ffdd8
Sallie McKee <salliemckee7@gmail.com>
2023-01-20 14:05:08 -0600

    switching over to reducers

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -2,7 +2,7 @@ import { createContext, useState, useEffect } from "react";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const CategoriesContext = createContext({
-  categoriesMap: [],
+  categoriesMap: {},
 });
 
 export const CategoriesProvider = ({ children }) => {
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 import {
   onAuthStateChangedListener,
@@ -11,12 +12,54 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  console.log("dispatched");
+  console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state, //spreads in previous values on state object, then changes the one you specify to change
+        currentUser: payload,
+      };
+    case "increment":
+      return {
+        value: state.value + 1,
+      };
+
+    default:
+      throw new Error(`unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null, //when we 1st initialize our app, we have no current user
+};
+
 // the actual component
 // essentially allowing any of its child components to access the values inside of its useState
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  //dispatch is a function that you pass an action object into. if you want the reducer to receive an action,
+  //you have to call dispatch
+
+  const { currentUser } = state;
+  console.log(currentUser);
+
+  const setCurrentUser = (user) => {
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+  };
+
+  // const [currentUser, setCurrentUser] = useState(null);
+
   // we want to be able to call this setter and get the value anywhere in the
   //component tree that is nested w/in the actual provider value
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
@@ -33,3 +76,21 @@ export const UserProvider = ({ children }) => {
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+/*
+Reducers are pretty much just functions that return an object
+
+const useReducer = (state, action) => {
+
+
+  return {
+currentUser: null
+
+  }
+  ***this object is the state of the reducer
+}
+
+
+
+
+*/
