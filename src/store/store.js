61f519eb02000dc41cdbb9e504db089f8dadf306
Sallie McKee <salliemckee7@gmail.com>
2023-01-25 13:57:20 -0600

    added redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { compose, createStore, applyMiddleware } from "redux";
 // import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
@@ -30,10 +32,24 @@ const loggerMiddleware = (store) => (next) => (action) => {
   console.log("next state: ", store.getState());
 };
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 
 //compose is a functional programming concept, it's a way for us to pass multiple functions left to right
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 //middleware has to be passed as the third argument
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
